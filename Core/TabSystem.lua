94d58e20fb64d6b9a4aa8dccbe206b48c715f480
LxckStxp <lxckstxp@gmail.com>
2025-02-13 11:05:56 +1000

    Update TabSystem.lua

--- Core/TabSystem.lua
+++ Core/TabSystem.lua
@@ -26,7 +26,7 @@ local contentArea
 -- Private Functions
 local function createTabButton(name)
     local button = Utils.Create("TextButton", {
-        Name = name .. "Button",
+        Name = tostring(name),
         Size = UDim2.new(0, 100, 1, -4),
         BackgroundColor3 = Styles.Colors.Controls.Button.Default,
         Text = name,
